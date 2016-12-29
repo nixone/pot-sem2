0048e4aa5a1e7e348250e176f8b525c1e85439ea
Martin Olesnanik <martinolesnanik@gmail.com>
2016-12-29 19:21:05 +0100

    Initial commit with few prototypes

---
+++ pot-sem2/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pot_sem2
+{
+
+    public enum Figure
+    {
+        MAN, KING, NONE
+    }
+
+    public enum Player
+    {
+        WHITE, BLACK, NONE
+    }
+
+    public class Field
+    {
+        public Player Player = Player.NONE;
+        public Figure Figure = Figure.NONE;
+    }
+
+    public class GameState
+    {
+        public Field[,] Board = new Field[8,8];
+    }
+
+    public class Game
+    {
+        private Random random = new Random();
+
+        public GameState GetGameState()
+        {
+            GameState result = new GameState();
+            int x = random.Next(8);
+            int y = random.Next(8);
+            result.Board[x, y].Player = Player.WHITE;
+            result.Board[x, y].Figure = Figure.MAN;
+            return result;
+        }
+    }
+}
---
+++ pot-sem2/WCFPrototype.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace pot_sem2
+{
+    class WCFPrototype
+    {
+        public WCFPrototype()
+        {
+
+        }
+
+        public void Run()
+        {
+            Thread serverThread = new Thread(RunServer);
+            serverThread.Start();
+        }
+
+        public void RunServer()
+        {
+            ServiceHost host = new ServiceHost(typeof(SampleService), new Uri[] { new Uri("net.tcp://localhost:8123/sample") });
+            try
+            {
+                host.Open();
+                Thread clientThread = new Thread(RunClient);
+                clientThread.Start();
+                Thread client2Thread = new Thread(RunClient);
+                client2Thread.Start();
+                clientThread.Join();
+                client2Thread.Join();
+                host.Close();
+            }
+            catch (CommunicationException e)
+            {
+                Console.WriteLine("There was some problem! {0}", e.Message);
+                host.Abort();
+            }
+        }
+
+        public void RunClient()
+        {
+            using (var channelFactory = new ChannelFactory<ISampleService>("pot_sem2.SampleService", new EndpointAddress("net.tcp://localhost:8123/sample")))
+            {
+                ISampleService service = channelFactory.CreateChannel();
+
+                Console.WriteLine(service.GetHelloMessage("World"));
+            }
+        }
+    }
+}
